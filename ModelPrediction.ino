a8ac1107104d30f29010c4c64409e4e9a55f67a5
oezeb <eouedraogo4@gmail.com>
2025-10-24 17:45:53 +0800

    add c code

--- ModelPrediction.ino
+++ ModelPrediction.ino
@@ -1,6 +1,9 @@
 #include "MyBoschSensor.h"
 #include "SignalQueue.h"
 #include "features.h"
+#include "xgb_mlgen.h"
+
+Eloquent::ML::Port::XGBClassifier xgb;
 
 void handle_signal() {
   float ax, ay, az, gx, gy, gz;
@@ -31,28 +34,31 @@ void setup() {
   Serial.println(" Hz");
   Serial.println("Gyroscope in degrees/second");
   Serial.println();
+  
+  Serial.println("time,predicted-class,feature-extraction-time-us,prediction-time-us");
 }
 
 float ax[WINDOW], ay[WINDOW], az[WINDOW], gx[WINDOW], gy[WINDOW], gz[WINDOW];
 float features[120]; // 20 features * 6 axes
+char output[128];
 void loop() {
-  if (signal_queue.is_full()) {
+  while (signal_queue.is_full()) {
     signal_queue.peek(ax, ay, az, gx, gy, gz);
+
+    ulong start = micros();
     extract_features(ax, WINDOW, &features[0]);
     extract_features(ay, WINDOW, &features[20]);
     extract_features(az, WINDOW, &features[40]);
     extract_features(gx, WINDOW, &features[60]);
     extract_features(gy, WINDOW, &features[80]);
     extract_features(gz, WINDOW, &features[100]);
-    Serial.println("Extracted features:");
-    for (int i = 0; i < 120; i++) {
-      Serial.print(features[i], 6);
-      if (i < 119) {
-        Serial.print(", ");
-      }
-    }
-    Serial.println();
+    
+    ulong start2 = micros();
+    int y_pred = xgb.predict(features);
+    ulong end = micros();
+    snprintf(output, sizeof(output), "%lu,%d,%lu,%lu", start, y_pred, start2 - start, end - start2);
+    Serial.println(output);
 
-    delay(1000);
+    // delay(1000);
   }
 }
